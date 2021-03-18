852360be6516d27b2b12a2299c581e5c9f6b61a6
Venkat Rangan <venkat@tinyvision.ai>
2021-03-18 13:15:58 -0700

    Cleaned up code, added programmability

--- SoM/RTL/vision/sw/read_himax.py
+++ SoM/RTL/vision/sw/read_himax.py
@@ -1,41 +1,49 @@
-from __future__ import division
 import serial
 import time
-import threading
 import numpy as np
-import matplotlib.pyplot as plt
-from matplotlib.animation import FuncAnimation
+import cv2
+import argparse
 
 
-ser = serial.Serial(port='COM7', baudrate=460800, timeout=1.0)
-ser.set_buffer_size(rx_size = 1000000, tx_size = 12800)
+parser = argparse.ArgumentParser(prog='read_himax', description='Himax image sensor downloader')
+parser.add_argument('--baud', type=int, default=460800)
+parser.add_argument('-l', '--loop', action='store_true')
+parser.add_argument('-o', '--out', default='img')
+parser.add_argument('port', help='Windows: COMx, /dev/ttyUSBx on Linux')
+
+args = parser.parse_args()
+
+ser = serial.Serial(port=args.port, baudrate=args.baud, timeout=1.0)
 
 print(ser.name)
 ser.flushInput()
 ser.flushOutput()
 
-# "Warm up" the AEC
-#ser.write(b'x')
-#ser.write(b'x')
-#time.sleep(1)
-
-#plt.ion()
+# "Warm up" the board by acquiring a frame
+ser.write(b'x')
+time.sleep(1)
 
 (cols, rows) = (162, 162)
 #(cols, rows) = (324, 324)
 
-# Throw away bad pixels
+frameno = 0
 while(True):
+    print('Frame no: %d' % (frameno) )
     ser.flushInput()
     ser.write(b'x')
     resp = ser.read(100000) # Max length to be read is a frame
-    image = np.asarray(list(resp))
-
-    rows = int(np.floor(len(image)/cols))
-    print(rows)
-    image = image[0:rows*cols]
-    image = image.reshape(rows, cols)
-
-    plt.imshow(image, cmap='gray', vmin=0, vmax=255)
-    plt.show()
-    #time.sleep(0.1)
+    print(len(resp))
+    if (len(resp)>0):
+        image = np.frombuffer(resp, dtype=np.uint8).reshape(rows, -1)
+        cv2.imshow('capture', image)
+        k = cv2.waitKey(0)
+        if args.loop and args.out:
+            if frameno < 9999:
+                fname = "%s-%04d.tiff" %(args.out, frameno)
+                cv2.imwrite(fname, image)
+            else:
+                print("Exceeded frame count")
+        if k == 27:         # wait for ESC key to exit
+            cv2.destroyAllWindows()
+            break
+    frameno = frameno + 1
